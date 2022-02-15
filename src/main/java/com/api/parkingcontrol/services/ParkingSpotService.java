23831973d4d28ca7f48c9d76433d55733d86c0f1
Ivan de Siqueira <ivandesiqueira@gmail.com>
2022-02-15 20:18:00 -0300

    Implementando classe DTO

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,18 @@
 package com.api.parkingcontrol.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
 public class ParkingSpotService {
+
+	@Autowired
+	ParkingSpotRepository parkingSpotRepository;
+	
+	
+	
+	
 
 }
