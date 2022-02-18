d6ba5f6354e085af4ee366c96acbac2d1a01eb0c
Ivan de Siqueira <ivandesiqueira@gmail.com>
2022-02-18 20:49:03 -0300

    Add bild path

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -58,7 +58,7 @@ public class ParkingSpotController {
 	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
 		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
 		if (!parkingSpotModelOptional.isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found!");
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
@@ -70,7 +70,7 @@ public class ParkingSpotController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
 		}
 		parkingSpotService.delete(parkingSpotModelOptional.get());
-		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully.");
+		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully!");
 	}
 
 	@PutMapping("/{id}")
